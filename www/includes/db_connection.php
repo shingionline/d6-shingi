b8161ddc3d18028a3fd80007872157136737a642
Shingi Mushipe <shingi2009@gmail.com>
2023-02-10 06:46:17 +0200

    Updated error message

--- www/includes/db_connection.php
+++ www/includes/db_connection.php
@@ -9,5 +9,7 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
     echo "Database connection failed: " . $e->getMessage();
+    echo "<p>Don't worry, I experienced this on my local machine. Most likely Docker is still booting up.</p>";
+    echo "<p>Please <a href='http://localhost:8001'>try again</a> after a few seconds.</p>";
     die();
 }
